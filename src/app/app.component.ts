d602b81e29b4b08d67fe367ec4ef401419fe43e1
Jan Forgac <jan.forgac@educanet.cz>
2020-11-30 00:01:53 +0100

    sidebar + header components + styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'muj prvni angular test';
-  counter = 0;
 
-  increaseCounter(): void {
-    this.counter++;
-  }
 }
