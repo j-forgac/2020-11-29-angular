0314674984e917b6a23cf36a424235fc9bd3a740
Jan Forgac <jan.forgac@educanet.cz>
2020-12-06 15:14:18 +0100

    vyhledávání s filtrací

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,9 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Routes} from '@angular/router';
+import {HomeComponent} from '../home/home.component';
+import {AboutComponent} from '../about/about.component';
+import {ContactsComponent} from '../contacts/contacts.component';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +13,13 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 export class HeaderComponent implements OnInit {
   private visibile = true;
+  search = '';
+
   @Output() visibilityChanged = new EventEmitter<boolean>();
   @Input() menu = {};
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -20,4 +28,15 @@ export class HeaderComponent implements OnInit {
     this.visibile = !this.visibile;
     this.visibilityChanged.emit(this.visibile);
   }
+
+  /* metoda neaplikována kvůli datalistu
+  printResults(): void {
+    let myVal: string;
+    for (const [key, value] of Object.entries(this.menu)) {
+      myVal = String(value).toLowerCase();
+      if (myVal.includes(this.search.toLowerCase())){
+        console.log(value);
+      }
+    }
+    */
 }
