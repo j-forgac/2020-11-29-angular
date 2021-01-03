fdc75bc4ced9adf8344a8998fad504a7a8a5cb61
Jan Forgac <jan.forgac@educanet.cz>
2021-01-03 23:39:14 +0100

    edit post

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -7,8 +7,13 @@ import {BlogPost} from './BlogPost';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
+  counter = 0;
   title = '';
   text = '';
+  updatedTitle = '';
+  updatedText = '';
+  vis = false;
+  editedPost: BlogPost | undefined;
   blogPosts: BlogPost[] = [];
 
   constructor() { }
@@ -21,6 +26,30 @@ export class BlogComponent implements OnInit {
     console.log(this.title);
     console.log(this.text);
     this.blogPosts.push(new BlogPost(this.title, this.text));
+    this.counter++;
+    this.title = '';
+    this.text = '';
+  }
+
+  showForm(): void{
+    this.vis = true;
+    console.log('current post: ');
+    console.log(this.editedPost);
+    if (this.editedPost !== undefined){
+      this.updatedTitle = this.editedPost.title;
+      this.updatedText = this.editedPost.text;
+    }
+  }
+
+  hideForm(): void{
+    this.vis = false;
+  }
+
+  updatePost(): void{
+    // @ts-ignore
+    this.editedPost.title = this.updatedTitle;
+    // @ts-ignore
+    this.editedPost.text = this.updatedText;
   }
 
 }
